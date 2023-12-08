ba3025003314980c71fc788b8ab0cdea3956596e
Koro0 <ledane92@gmail.com>
2023-12-08 18:03:58 +0100

    create product model

---
+++ models/Product.models.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  productId: { type: String, required: true },
+  name: { type: String, required: true },
+  productSerie: { type: String, required: true },
+  caloris: { type: Number, required: true },
+  glucides: { type: Number, required: true },
+  sucres: { type: Number, required: true },
+  matieresGrasses: { type: Number, required: true },
+  acideGrasSaturer: { type: Number, required: true },
+  fibre: { type: Number, required: true },
+  proteines: { type: Number, required: true },
+  sel: { type: Number, required: true },
+});
+export default productSchema;
