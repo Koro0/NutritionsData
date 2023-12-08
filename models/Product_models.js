c1be61385f798e901df65f12ec08b64e913cb2fe
Koro0 <ledane92@gmail.com>
2023-12-08 18:21:59 +0100

    clean code

--- models/Product_models.js
+++ models/Product_models.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-  productId: { type: String, required: true },
   name: { type: String, required: true },
   imageUrl: { type: String, required: true },
   productSerie: { type: String, required: true },
